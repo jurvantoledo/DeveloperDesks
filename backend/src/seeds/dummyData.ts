bd0f7057651860284a20e661c9af83a60c0a0acd
Matias Garcia <matiasigarcia91@gmail.com>
2020-10-01 16:03:18 +0200

    hash dummyData passwords

--- backend/src/seeds/dummyData.ts
+++ backend/src/seeds/dummyData.ts
@@ -1,9 +1,12 @@
+import * as bcrypt from "bcrypt";
+
 export const seedData = [
   {
     uri:
       "https://res.cloudinary.com/daxjf54p3/image/upload/v1601306412/hmvcvmdpj3dqgca4vtqk.jpg",
     developer: {
       name: "Michiel",
+      password: bcrypt.hashSync("test", 10),
       email: "michiel@codaisseur.com",
     },
   },
@@ -12,6 +15,7 @@ export const seedData = [
       "https://res.cloudinary.com/daxjf54p3/image/upload/v1601299132/pgzw8qoegk56qbnpatac.jpg",
     developer: {
       name: "Matias",
+      password: bcrypt.hashSync("test", 10),
       email: "matias@codaisseur.com",
     },
   },
@@ -20,6 +24,7 @@ export const seedData = [
       "https://res.cloudinary.com/daxjf54p3/image/upload/v1601299126/kdcdh6jtrw1qwwpjopjt.jpg",
     developer: {
       name: "Hande & Joost",
+      password: bcrypt.hashSync("test", 10),
       email: "hande+joost@codaisseur.com",
     },
   },
@@ -28,6 +33,7 @@ export const seedData = [
       "https://res.cloudinary.com/daxjf54p3/image/upload/v1601299119/dpsbdc0hq2qr9jebk5zh.jpg",
     developer: {
       name: "Maria",
+      password: bcrypt.hashSync("test", 10),
       email: "maria@codaisseur.com",
     },
   },
@@ -36,6 +42,7 @@ export const seedData = [
       "https://res.cloudinary.com/daxjf54p3/image/upload/v1601298639/dmi3ccpbwbauyotcx5ui.jpg",
     developer: {
       name: "Alex",
+      password: bcrypt.hashSync("test", 10),
       email: "alexandra@codaisseur.com",
     },
   },
